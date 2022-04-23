5e650714a05a970c7250d73dfd7e43aefe41f347
Mekre-A <mekre.abate@gmail.com>
2022-04-23 11:41:11 +0300

    Added some styles to the success view

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -1,18 +1,23 @@
 import { useLocation } from "react-router-dom";
-
+import "./success.css";
 function Success() {
   const location = useLocation();
   const state = location.state as any;
   console.log("Inside Location");
   return (
-    <div>
-      <p>Thanks {state.firstName}! We've received your application</p>
-      <p>
+    <div className="success-container">
+      <div className="success-vertical-spacer"></div>
+      <p className="success-paragraph success-paragraph-bold">
+        Thanks {state.firstName}! We've received your application
+      </p>
+      <div className="success-vertical-spacer-small"></div>
+      <p className="success-paragraph">
         We'll process your application as soon as possible and send you a
         decision with 30 days to {state.phoneNumber} or {state.emailAddress}. We
         will contact you in case more information is needed.
       </p>
-      <p>
+      <div className="success-vertical-spacer-small"></div>
+      <p className="success-paragraph">
         While we're reviewing your application, please don't submit another
         application for the uPet's breeder program.
       </p>
